d67b5b66404403a04b2c0e9b5efc3f9bf0648312
Raihan <tipuraihanff@gmail.com>
2026-03-30 17:14:53 +0600

    Update Search.jsx

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -147,13 +147,6 @@ export default function Search() {
               autoComplete="off"
               spellCheck="false"
             />
-            {searchQuery && (
-              <button className="srh-clear" onClick={() => setSearchQuery('')} title="Clear">
-                <svg width="14" height="14" fill="none" stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24">
-                  <line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/>
-                </svg>
-              </button>
-            )}
             <button 
               className="srh-search-btn"
               onClick={handleSearch}
